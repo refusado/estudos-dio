d7ef2d6ebbd6b5876268dcf052d9fc07e6f47881
Renan Freitas <89546855+Refusado@users.noreply.github.com>
2022-06-30 05:54:54 -0300

    deletando pasta antiga (da alteração de nome)

--- introducao-ao-javascript/error-handling/error-object.js
+++
@@ -1,8 +0,0 @@
-// new Error(message, fileName, lineNumber);
-
-// Todos os parâmentros são opcionais
-
-const meuErro = new Error("Mensagem inválida :/");
-meuErro.name = "NomeDoErro"; // opcional
-
-throw meuErro;
