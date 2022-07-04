acf419cb7416176718e410f2a878749d1a16fb39
Refusado <orefusado@gmail.com>
2022-07-04 20:47:35 -0300

    aula de assincronicidade

---
+++ javascript/assincronicidade/just-cats/atividade.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById('change-cat');
+const img = document.getElementById('cat');
+const loading = document.getElementById('loading');
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search/';
+
+const pegaGato = async () => {
+  try {
+
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+    
+    return json[0].url;
+  }
+  catch (e) {
+    console.log(e.message);
+  }
+}
+
+const loadImg = async () => {
+  loading.style.display = 'block';
+
+  img.src = await pegaGato();
+  
+  img.addEventListener('load', function() {
+    loading.style.display = 'none';
+  })
+}
+
+btn.addEventListener("click", loadImg);
+
+loadImg();
